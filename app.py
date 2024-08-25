5c7c72797d802264e4688f87fce70f5d731739d1
Hiroyuki_Igusa_mac11 <igusahiroyuki@igusanoMacBook-Air.local>
2024-08-25 10:56:23 +0900

    gspread added

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-# example_app.py
 import streamlit as st
 
-st.title('Hello, Streamlit!')
-st.write('これはサンプルのStreamlitアプリです。')
+from google.oauth2.service_account import Credentials
+import gspread
+
+st.write("TEST")
+
+# スコープを指定
+scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+# サービスアカウントのJSONファイルを使用して認証情報を作成
+creds = Credentials.from_service_account_file(
+    'env/my-project-eiken-587808610231.json',
+    scopes=scopes
+)
+
+# gspreadクライアントを作成
+client = gspread.authorize(creds)
+
+# スプレッドシートの取得
+spreadsheet_id = '14PmuhBLAv54cUmYeQfo2BqwJHe8FQWUIaZAoJry78So'  # スプレッドシートIDを設定
+spreadsheet = client.open_by_key(spreadsheet_id)
+
+# ワークシートの取得
+worksheet = spreadsheet.sheet1  # 最初のワークシートを取得
+data = worksheet.get_all_records()  # データを取得
+st.write(data)
